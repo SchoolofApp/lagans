7735d812f9386da23b2cb300617c32068eb0333e
shyamkryadav <shyamkryadav345@gmail.com>
2022-08-07 17:17:22 +0545

    header and footer is maked

---
+++ footer.php
@@ -0,0 +1,10 @@
+
+<footer class="m-2">
+    <p>Author: Hege Refsnes<br>
+    <a href="mailto:hege@example.com">hege@example.com</a></p>
+  </footer>
+
+<h3>shyam </h3>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lagan</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/style.css">
+</head>
+<body>
+    
+<nav class="navbar navbar-expand-sm navbar-dark bg-primary">
+    <div class="container-fluid   navbar-expand-lg ">
+      <a class="navbar-brand " href="javascript:void(0)">Lagan</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="mynavbar">
+        <ul class="navbar-nav me-auto">
+            <form class="d-flex ms-xl-5 "  >
+                <input class="form-control me-2 " type="text" placeholder="Search">
+                <button class="btn btn-primary" type="button">Search</button>
+              </form>
+          <li class="nav-item">
+            <a class="nav-link ms-5 active" href="javascript:void(0)">Alert</a>
+          </li>
+       
+
+        </ul>
+
+   
+       
+          <span class="dot"></span>
+
+      </div>
+    </div>
+  </nav>
+  <!-- end header -->
